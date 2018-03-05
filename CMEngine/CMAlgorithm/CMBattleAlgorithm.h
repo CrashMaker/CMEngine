f8d874230c2f9d296128478e54209a8c7c620854
CrashMaker <shiys2012@sina.cn>
2018-03-05 10:46:03 +0800

    添加CMAlgorithm

---
+++ CMEngine/CMAlgorithm/CMBattleAlgorithm.h
@@ -0,0 +1,22 @@
+/*================================================================
+*   Copyright (C) 2018 CrashMaker. All rights reserved.
+*   
+*   文件名称：CMBattleAlgorithm.h
+*   创 建 者：CrashMaker
+*   创建日期：2018年03月05日
+*   描    述：
+*
+================================================================*/
+
+#ifndef CMBATTLEALGORITHM_H
+#define CMBATTLEALGORITHM_H
+
+class CMBattleAlgorithm
+{
+private:
+
+public:
+
+};
+
+#endif /* CMBATTLEALGORITHM_H */
