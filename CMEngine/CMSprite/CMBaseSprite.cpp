9593d6c1bb8b6733086bad1c4d93cb0d67eed6e2
CrashMaker <shiys2012@sina.cn>
2018-03-20 09:40:09 +0800

    开发精灵中

--- CMEngine/CMSprite/CMBaseSprite.cpp
+++ CMEngine/CMSprite/CMBaseSprite.cpp
@@ -10,4 +10,41 @@
 
 #include "CMBaseSprite.h"
 
+#define MaxLevel 99         // 最高等级
 
+namespace cmengine
+{
+    void CMBaseSprite::SetLevel(int lv)
+    {
+        if (lv <= 0) {
+            level = 1;
+        } else if (lv > MaxLevel) {
+            level = MaxLevel;
+        } else {
+            level = lv;
+        }
+
+        SetupInnateAttribute();
+    }
+
+    void CMBaseSprite::SetupInnateAttribute()
+    {
+        innateAttribute.Reset();
+        innateAttribute.attack += (growthRate.attackGrowthRate * level);
+        innateAttribute.defense += (growthRate.defenseGrowthRate * level);
+        innateAttribute.magicAtk += (growthRate.magicAtkGrowthRate * level);
+        innateAttribute.magicDef += (growthRate.magicDefGrowthRate * level);
+        innateAttribute.health += (growthRate.healthGrowthRate * level);
+        innateAttribute.speed += (growthRate.speedGrowthRate * level);
+
+        SetupCurrentAttribute();
+    }
+
+    void CMBaseSprite::SetupCurrentAttribute()
+    {
+        currentAttribute = innateAttribute;
+
+        // 恢复生命值
+        healthPoint = currentAttribute.health;
+    }
+}
--- CMEngine/CMSprite/CMBaseSprite.h
+++ CMEngine/CMSprite/CMBaseSprite.h
@@ -16,17 +16,72 @@
 
 namespace cmengine
 {
+    // 角色成长率，建议攻击、防御、生命以2:1:3的比例成长
+    struct CMSpriteGrowthRate
+    {
+        float attackGrowthRate;
+        float defenseGrowthRate;
+        float magicAtkGrowthRate;
+        float magicDefGrowthRate;
+        float healthGrowthRate;
+        float speedGrowthRate;
+    };
+    
+    // 角色基础属性, 默认都为1
+    struct CMSpriteBaseAttribute
+    {
+        int attack;
+        int defense;
+        int magicAtk;
+        int magicDef;
+        int health;
+        int speed;
+
+        CMSpriteBaseAttribute() 
+            : attack(1), defense(1), magicAtk(1),
+              magicDef(1), health(1), speed(1) {}
+
+        void Reset() 
+        {
+            attack = 1;
+            defense = 1;
+            magicAtk = 1;
+            magicDef = 1;
+            health = 1;
+            speed = 1;
+        }
+    };
+
     class CMBaseSprite
     {
     public:
-        CMBaseSprite ();
+        CMBaseSprite (std::string name_, CMSpriteGrowthRate growthRate_) 
+            : name(name_), growthRate(growthRate_) 
+        {
+            stateType = SpriteStateTypeLife;
+            SetLevel(1);
+        }
         virtual ~CMBaseSprite () {}
     
         std::string GetName() const {return name;}
+        int GetLevel() const {return level;}
+
+        void SetLevel(int lv);
+        void SetupInnateAttribute();
+        void SetupCurrentAttribute();
     private:
         std::string name;
+        CMSpriteGrowthRate growthRate;
+
+        // 固有属性，只会因为等级变化而改变。
+        CMSpriteBaseAttribute innateAttribute;
+        // 当前属性，会随着固有属性、装备或附加状态而改变。
+        CMSpriteBaseAttribute currentAttribute;
+        // 等级
         int level;
+        // 当前生命值，无法高于最大生命值，当为0时，角色进入死亡状态。
         int healthPoint;
+        // 角色状态
         SpriteStateType stateType;
     };
 }
