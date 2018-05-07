3ae051e510240385b24fff21e46d08f9e3f84baf
CrashMaker <shiys2012@sina.cn>
2018-05-07 15:08:58 +0800

    ChooseSkillIndexFromSprite

--- CMEngine/CMBattle/CMBattleChoose.cpp
+++ CMEngine/CMBattle/CMBattleChoose.cpp
@@ -9,13 +9,34 @@
 ================================================================*/
 
 #include "CMBattleChoose.h"
+#include "../CMSourceManager/CMInstantiateSource.h"
+#include <iostream>
 
 namespace cmengine
 {
     // 选择角色的技能，返回技能编号
     int CMBattleChoose::ChooseSkillIndexFromSprite(CMBaseSprite* sprite)
     {
-        return sprite->GetSkillList()[0];
+        int result = 0;
+        bool action = true;
+        do {
+            std::cout << "选择技能：";
+            for (int i = 0; i < (int)sprite->GetSkillList().size(); ++i) {
+                BaseSkill sk = CMInstantiateSource::InstantiateSkill(sprite->GetSkillList()[i]);
+                std::cout << i+1 << "." << sk->GetName() << " "; 
+            }
+            std::cout << std::endl;
+            
+            std::cin >> result;
+            result -= 1;
+            if (result >= 0 && result < (int)sprite->GetSkillList().size()) {
+                action = false;
+            } else {
+                std::cout << "输入错误，请重新选择" << std::endl;
+            }
+        } while(action);
+        
+        return sprite->GetSkillList()[result];
     }
 
     /* ==========实现的协议========== */
