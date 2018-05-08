de373dfaa9cd514d86699c0d7b74738032f1c1c5
CrashMaker <shiys2012@sina.cn>
2018-05-08 11:34:12 +0800

    提交

--- CMEngine/CMBattle/CMBattleChoose.cpp
+++ CMEngine/CMBattle/CMBattleChoose.cpp
@@ -44,7 +44,35 @@ namespace cmengine
     // 根据施法者获取指定的队伍
     std::vector<CMBaseSprite*> CMBattleChoose::GetTeamWithCaster(CMBaseSprite* sprite, BattleChooseTeam chooseTeam)
     {
+        std::vector<CMBaseSprite*> allTeam;
+        allTeam.insert(allTeam.end(), firstTeam.begin(), firstTeam.end());
+        allTeam.insert(allTeam.end(), secondTeam.begin(), secondTeam.end());
+        if (BattleChooseTeamOfAll == chooseTeam) {return allTeam;}
 
+        std::vector<CMBaseSprite*> selfTeam;
+        std::vector<CMBaseSprite*> anotherTeam;
+
+        std::vector<CMBaseSprite*>::iterator it_1;
+        it_1 = std::find(firstTeam.begin(), firstTeam.end(), sprite);
+        if (it_1 != firstTeam.end()) {
+            selfTeam = firstTeam;
+            anotherTeam = secondTeam;
+        }
+
+        std::vector<CMBaseSprite*>::iterator it_2;
+        it_2 = std::find(secondTeam.begin(), secondTeam.end(), sprite);
+        if (it_2 != secondTeam.end()) {
+            selfTeam = secondTeam;
+            anotherTeam = firstTeam;
+        }
+
+        if (BattleChooseTeamOfSelf == chooseTeam) {
+            return selfTeam;
+        } else if (BattleChooseTeamOfAnother == chooseTeam) {
+            return anotherTeam;
+        } else {
+            return allTeam;
+        }
     }
 
     /* ==========实现的协议========== */
@@ -52,7 +80,26 @@ namespace cmengine
     // 选取目标
     CMBaseSprite* CMBattleChoose::ObtainTarget(CMBaseSprite* caster)
     {
+        std::vector<CMBaseSprite*> targetTeam = GetTeamWithCaster(caster, BattleChooseTeamOfAnother);
+        int result = 0;
+        bool action = true;
+        do {
+            std::cout << "选择目标：";
+            for (int i = 0; i < (int)targetTeam.size(); ++i) {
+                CMBaseSprite* sp = targetTeam[i];
+                std::cout << i+1 << "." << sp->GetName() << " "; 
+            }
+            std::cout << std::endl;
+            
+            std::cin >> result;
+            result -= 1;
+            if (result >= 0 && result < (int)targetTeam.size()) {
+                action = false;
+            } else {
+                std::cout << "输入错误，请重新选择" << std::endl;
+            }
+        } while(action);
         
-        return secondTeam[0];
+        return targetTeam[result];
     }
 }
