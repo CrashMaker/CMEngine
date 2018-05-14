e599740b5977f1fa9c2fe27e13bbbf7bf65d752f
CrashMaker <shiys2012@sina.cn>
2018-05-14 14:39:59 +0800

    添加CMBaseAnnex

---
+++ CMEngine/CMAnnex/CMBaseAnnex.h
@@ -0,0 +1,26 @@
+/*================================================================
+*   Copyright (C) 2018 CrashMaker. All rights reserved.
+*   
+*   文件名称：CMBaseAnnex.h
+*   创 建 者：CrashMaker
+*   创建日期：2018年05月14日
+*   描    述：附属于角色的附加属性
+*
+================================================================*/
+
+#ifndef CMBASEANNEX_H
+#define CMBASEANNEX_H
+
+namespace cmengine
+{
+    class CMBaseAnnex
+    {
+    public:
+        virtual ~CMBaseAnnex() {}
+    
+    private:
+        /* data */
+    };
+}
+
+#endif /* CMBASEANNEX_H */
